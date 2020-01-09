ab2a85d808278696ef303752201ebf4f75eb48a0
Ekawahyu Susilo <ekawahyu@yahoo.com>
2020-01-09 14:02:52 -0800

    updated server.js

--- server.js
+++ server.js
@@ -1,22 +1,11 @@
 const usb = require('usb');
-var async = require('async');
+const async = require('async');
 
-var isLinux  = process.platform === 'linux';
-var isDarwin = process.platform === 'darwin';
-var isWin    = process.platform === 'win32';
+const isLinux  = process.platform === 'linux';
+const isDarwin = process.platform === 'darwin';
+const isWin    = process.platform === 'win32';
 
 const shell = require('shelljs')
-
-console.log('Run once');
-if (isLinux) {
-  shell.exec('./devupdate_linux.sh')
-}
-if (isDarwin) {
-  shell.exec('./devupdate_darwin.sh')
-}
-if (isWin) {
-  console.log('OS not supported yet');
-}
 
 usb.on('attach', function(device) {
   console.log('USB plugged in');
@@ -43,3 +32,16 @@ usb.on('detach', function(device) {
     console.log('OS not supported yet');
   }
 });
+
+console.log('Run once');
+if (isLinux) {
+  shell.exec('./devupdate_linux.sh')
+}
+if (isDarwin) {
+  shell.exec('./devupdate_darwin.sh')
+}
+if (isWin) {
+  console.log('OS not supported yet');
+}
+
+
